ad0fb4a0a7611802deed8610c21893a50649d23f
Shawn Webster <swolfgangwebster@gmail.com>
2023-05-07 15:32:33 -0500

    ihatethis

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(a,b) {
+    let user = {name: a, email: b}
+
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+
+    .then(resp => resp.json())
+    .then(r => document.querySelector('h2').append(r.id))
+    .catch(err => document.querySelector('h2').append(err))
+
+
+}
+
+document.querySelector('#submit').addEventListener('click', handle)
+
+function handle(e) {
+    e.preventDefault()
+    a = document.querySelector("#name").value
+    b = document.querySelector('#email').value
+    submitData(a,b)
+    document.querySelector("#name").value = ''
+    document.querySelector('#email').value = ''
+}
